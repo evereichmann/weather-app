6dd64da7c85872cf8aaa8944c2e9da2370d62425
evereichmann <eve.reichmann@gmail.com>
2022-01-11 15:44:37 -0600

    hid api keys

--- src/App.js
+++ src/App.js
@@ -3,33 +3,32 @@ import './App.css';
 
 function App() {
   const api = {
-    key: "6c5b15d18aea610d4e65f294c8f8f269",
+    key: `${process.env.REACT_APP_WEATHER_API_KEY}`,
     base: "https://api.openweathermap.org/data/2.5/"
   }
-
   const [query, setQuery] = useState ('');
   const [weather, setWeather] = useState ({});
-
+  
   const search = evt => {
     if(evt.key === "Enter") {
       fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
-        .then(resp => resp.json())
-        .then(result => {
-          setQuery('');
-          setWeather(result);
-          console.log(result)
-        })
+      .then(resp => resp.json())
+      .then(result => {
+        setQuery('');
+        setWeather(result);
+        console.log(result)
+      })
     }
   }
-
-const currentDate = new Date();
-const date = `${currentDate.getMonth()+1}/${currentDate.getDate()}/${currentDate.getFullYear()}`;
   
+  const currentDate = new Date();
+  const date = `${currentDate.getMonth()+1}/${currentDate.getDate()}/${currentDate.getFullYear()}`;
+
   return (
     <div className="App">
       <main>
         <div className="search-box">
-          <input type="text" classname="search-bar" placeholder="Location..." onChange={e => setQuery(e.target.value)} value={query} onKeyPress={search}/>
+          <input type="text" className="search-bar" placeholder="Location..." onChange={e => setQuery(e.target.value)} value={query} onKeyPress={search}/>
         </div>
         {(typeof weather.main !="undefined") ? (
         <div className="data">
@@ -41,8 +40,10 @@ const date = `${currentDate.getMonth()+1}/${currentDate.getDate()}/${currentDate
           </div>
           <div className="temputure">
             <h1>CURRENT: {Math.round(weather.main.temp)}</h1>
-            <h3>HIGH : {Math.round(weather.main.temp_max)}</h3>
-            <h3>LOW: {Math.round(weather.main.temp_min)}</h3>
+            <div className='sub-temp'>
+              <h3>HIGH : {Math.round(weather.main.temp_max)}</h3>
+              <h3>LOW: {Math.round(weather.main.temp_min)}</h3>
+            </div>
           </div>
           <div className="weather">
             <h2>{weather.weather[0].main}</h2>
